9415c75eea1bcc7b93cfd308f7ccebeaea719f64
Rohan Jeevan Janrao <52947469+rohan030101@users.noreply.github.com>
2023-12-23 07:00:39 +0000

    Started New Project and html, css done

---
+++ Counter/main.js
@@ -0,0 +1,17 @@
+console.log('Scripted Loaded');
+
+var value = document.querySelector('#value');
+// console.log(value.textContent);
+
+var btns= document.querySelectorAll(".btn");
+// console.log(btns);
+
+
+btns.forEach(function (btn){
+    btn.addEventListener('click' ,function(e){
+        const style = e.currentTarget.
+    })
+});
+
+
+
