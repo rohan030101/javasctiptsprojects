9eb02285b88da5da9925b0f6cf20d6251bf4d999
rohan030101 <janrarohan2@gmail.com>
2023-12-25 15:31:06 +0530

    Started Reviews Project

---
+++ Reviews/app.js
@@ -0,0 +1,30 @@
+console.log("Scirpt Loaded");
+
+const reviews = [
+  {
+    id: 1,
+    name: "Rohan Janrao",
+    job: "Desktop Engineer",
+    text: "Dedicated Desktop Engineer with a strong background in maintaining and optimizing computer systems for optimal performance. Proficient in troubleshooting hardware and software issues, ensuring seamless functionality for end-users. Committed to delivering efficient IT solutions and providing exceptional technical support.",
+  },
+  {
+    id: 2,
+    name: "Ganesh Sale",
+    job: "Desktop Engineer",
+    text: "Dedicated Desktop Engineer with a strong background in maintaining and optimizing computer systems for optimal performance. Proficient in troubleshooting hardware and software issues, ensuring seamless functionality for end-users. Committed to delivering efficient IT solutions and providing exceptional technical support.",
+  },
+  {
+    id: 3,
+    name: "Nikhil Jakate",
+    job: "Desktop Engineer",
+    text: "Dedicated Desktop Engineer with a strong background in maintaining and optimizing computer systems for optimal performance. Proficient in troubleshooting hardware and software issues, ensuring seamless functionality for end-users. Committed to delivering efficient IT solutions and providing exceptional technical support.",
+  },
+  {
+    id: 4,
+    name: "Naresh Sir",
+    job: "Desktop Engineer",
+    text: "Dedicated Desktop Engineer with a strong background in maintaining and optimizing computer systems for optimal performance. Proficient in troubleshooting hardware and software issues, ensuring seamless functionality for end-users. Committed to delivering efficient IT solutions and providing exceptional technical support.",
+  },
+];
+
+console.log(reviews);
